e32e374fcb74160fb7e93dd0954f592b09f0bd3f
PiotrEBE <piotr@evidencebased.education>
2022-04-27 11:52:02 +0100

    New files

--- 01-starting-setup/src/components/UI/Card.js
+++ 01-starting-setup/src/components/UI/Card.js
@@ -1,8 +1,6 @@
 import "./Card.css";
 
 const Card = (props) => {
-  let abc;
-  let gfh;
   const classes = "card " + props.className;
   return <div className={classes}>{props.children}</div>;
 };
